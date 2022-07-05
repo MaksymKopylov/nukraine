f04fc157dc38f5c2d56de0fe283118683aa28535
sugarfreeeee <trpavliuk@gmail.com>
2022-07-05 18:43:55 +0300

    Tania's amazing job

--- script.R
+++ script.R
@@ -13,3 +13,16 @@ ggplot(survey, aes(x=volunteer)) +
 
 
 
+ggplot(survey2, aes(volunteer)) + 
+  geom_bar(aes(y = (..count..)/sum(..count..)), fill = "#FDDB52") + 
+  scale_y_continuous(labels=scales::percent) +
+  ylab("% of all respondents") +
+  scale_x_discrete(labels = c("Так, допомагав/-ла фізично", 
+                              "Так, допомагав/-ла фінансово", "Так, допомагав/-ла фізично і фінансово",
+                              "Ні, але планую фізично допомагати",
+                              "Ні, але планую долучатися до фінансової допомоги",
+                              "Ні але планую долучатися до фізичної і фінансової допомоги",
+                              "Ні, і не планую долучатися",
+                              "Важко відповісти"))
+  
+
